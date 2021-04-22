a8ac866f6573f585ccc24c0937423752c6f646fe
zavr1k <zavr35rus@gmail.com>
2021-04-22 11:21:11 +0300

    Step 6. Added Progress bar

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import requests
+from progress.bar import Bar
 from pathlib import Path
 
 from page_loader.storage import save_html_page, \
@@ -14,30 +15,38 @@ def download(url: str, destination: str) -> str:
     logging.debug('Checking destination: %s', destination)
     check_access(destination)
 
-    logging.debug('Performing request: %s', url)
     html_page_data = perform_request(url).text
 
     modified_page, page_path, file_folder_path, resources \
         = process_page(html_page_data, url, destination)
 
-    logging.debug('Resources list: \n%s', '\n\n'.join([
-        f'url: {resource_url} \ndestination: {resource_destination}'
-        for resource_url, resource_destination in resources
-        ]))
+    number_of_resources = len(resources)
 
-    save_html_page(modified_page, page_path)
+    logging.debug('Resources list (%s): \n%s', number_of_resources,
+                  '\n\n'.join([
+                      f'url: {resource_url} '
+                      f'\ndestination: {resource_destination}'
+                      for resource_url, resource_destination in resources
+                    ]))
 
-    if resources:
-        Path(file_folder_path).mkdir(exist_ok=True)
-        logging.info('Folder was created, path: %s', file_folder_path)
+    with Bar('Processing', max=number_of_resources + 1) as bar:
 
-        for resource_url, resource_destination in resources:
-            try:
-                download_file(resource_url, resource_destination)
-                logging.debug('Saving: %s', resource_destination)
-            except requests.exceptions.HTTPError as e:
-                logging.warning(str(e))
-                continue
+        save_html_page(modified_page, page_path)
+        bar.next()
+
+        if resources:
+            Path(file_folder_path).mkdir(exist_ok=True)
+            logging.info('Folder was created, path: %s', file_folder_path)
+
+            for resource_url, resource_destination in resources:
+                try:
+                    download_file(resource_url, resource_destination)
+                    logging.debug('Saving: %s', resource_destination)
+                except requests.exceptions.HTTPError as e:
+                    logging.warning(str(e))
+                    continue
+                finally:
+                    bar.next()
 
     return page_path
 
